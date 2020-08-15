04de0030e61ed3b7a778641586c70ab381fbc994
gaurav <270mehta@gmail.com>
2020-08-15 16:03:37 +0530

    Added Specialities

--- lib/Screens/homepage.dart
+++ lib/Screens/homepage.dart
@@ -1,4 +1,7 @@
+import 'package:Appointment_App/data/data.dart';
+import 'package:Appointment_App/model/specialities.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Homepage extends StatefulWidget {
   @override
@@ -6,16 +9,31 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  List<SpecialityModel> specialities;
+
+  @override
+  void initState() {
+    super.initState();
+
+    specialities = getSpeciality();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        brightness: Brightness.light,
+        iconTheme: IconThemeData(color: Colors.black87),
+      ),
+      drawer: Drawer(child: Container() // Populate the Drawer in the next step.
+          ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
         child: Column(
           children: <Widget>[
-            SizedBox(
-              height: 50,
-            ),
             Align(
               alignment: Alignment.topLeft,
               child: Text(
@@ -30,7 +48,6 @@ class _HomepageState extends State<Homepage> {
               height: 30,
             ),
             TextField(
-                //scrollPadding: EdgeInsets.symmetric(vertical: 10),
                 decoration: InputDecoration(
                     fillColor: Color(0xffF6F5F4),
                     filled: true,
@@ -38,17 +55,72 @@ class _HomepageState extends State<Homepage> {
                     border: InputBorder.none,
                     hintText: 'Search Doctors , Clinic...')),
             SizedBox(
-              height: 30,
+              height: 20,
             ),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                'Specialities',
-                style: TextStyle(
-                    color: Colors.black87,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600),
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Hospitals',
+                  style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600),
+                ),
+                FlatButton(
+                  child: Text('View All'),
+                  onPressed: () {
+                    Fluttertoast.showToast(
+                      msg: "Coming Soon",
+                      backgroundColor: Colors.blue,
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.CENTER,
+                    );
+                  },
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Specialities',
+                  style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600),
+                ),
+                FlatButton(
+                  child: Text('View All'),
+                  onPressed: () {
+                    Fluttertoast.showToast(
+                      msg: "Coming Soon",
+                      backgroundColor: Colors.blue,
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.CENTER,
+                    );
+                  },
+                ),
+              ],
+            ),
+            Container(
+              height: 200,
+              child: ListView.builder(
+                  itemCount: specialities.length,
+                  shrinkWrap: true,
+                  physics: ClampingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return SpecialistTile(
+                      imgAssetPath: specialities[index].imgAssetPath,
+                      speciality: specialities[index].speciality,
+                      noOfDoctors: specialities[index].noOfDoctors,
+                      backColor: specialities[index].backgroundColor,
+                    );
+                  }),
             ),
           ],
         ),
---
+++ lib/model/specialities.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SpecialityModel {
+  String imgAssetPath;
+  String speciality;
+  int noOfDoctors;
+  Color backgroundColor;
+  SpecialityModel(
+      {this.imgAssetPath,
+      this.speciality,
+      this.noOfDoctors,
+      this.backgroundColor});
+}
+
+class SpecialistTile extends StatelessWidget {
+  final String imgAssetPath;
+  final String speciality;
+  final int noOfDoctors;
+  final Color backColor;
+  SpecialistTile(
+      {@required this.imgAssetPath,
+      @required this.speciality,
+      @required this.noOfDoctors,
+      @required this.backColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 140,
+      margin: EdgeInsets.only(right: 16),
+      decoration: BoxDecoration(
+          color: backColor, borderRadius: BorderRadius.circular(24)),
+      padding: EdgeInsets.only(top: 16, right: 16, left: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            speciality,
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          SizedBox(
+            height: 6,
+          ),
+          Text(
+            "$noOfDoctors Doctors",
+            style: TextStyle(color: Colors.white, fontSize: 13),
+          ),
+          Image.asset(
+            imgAssetPath,
+            height: 117,
+            fit: BoxFit.fitHeight,
+          )
+        ],
+      ),
+    );
+  }
+}
