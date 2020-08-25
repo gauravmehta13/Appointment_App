6d3c7f3d47d6dd30780377e6f96f0fdfc4ba89b4
gaurav <270mehta@gmail.com>
2020-08-25 09:05:02 +0530

    Changes in User_profile

--- lib/Screens/user_profile.dart
+++ lib/Screens/user_profile.dart
@@ -7,30 +7,120 @@ class UserProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: <Widget>[
-        Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
-            Container(
-              color: Colors.blueGrey,
-              height: 300,
-            ),
-            Positioned(
-                child: CircleAvatar(
-              radius: 80,
-              backgroundImage: NetworkImage(
-                  'https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png'),
-            )),
-          ],
-        ),
-        ListTile(
-          leading: Icon(Icons.verified_user),
-          dense: true,
-          title: Text('Gaurav'),
-          subtitle: Text('No Apponitments Due'),
-        )
-      ],
+        body: Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: NetworkImage(
+                  'https://cdn2.vectorstock.com/i/1000x1000/31/16/abstract-honeycomb-pattern-geometric-background-vector-23363116.jpg'),
+              fit: BoxFit.none)),
+      child: Column(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 60, 20, 10),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  elevation: 50,
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 60,
+                          backgroundImage: NetworkImage(
+                              'https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png'),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Gaurav Yadav',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w400),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          '+91 7073142922',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w200),
+                        )
+                      ],
+                    ),
+                    decoration: BoxDecoration(
+                        gradient: new LinearGradient(
+                          colors: [
+                            const Color(0xFF3366FF),
+                            const Color(0xFF00CCFF),
+                          ],
+                          begin: const FractionalOffset(0.0, 0.0),
+                          end: const FractionalOffset(1.0, 0.0),
+                          stops: [0.0, 1.0],
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                    //color: Colors.blueGrey,
+                    height: 300,
+                    width: double.infinity,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 15, 20, 0),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  elevation: 50,
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: <Widget>[
+                          ListTile(
+                            leading: Icon(Icons.calendar_today),
+                            title: Text('Appoinments'),
+                            trailing: Icon(Icons.arrow_forward_ios),
+                          ),
+                          Divider(),
+                          ListTile(
+                            leading: Icon(Icons.video_call),
+                            title: Text('Online Consultation'),
+                            trailing: Icon(Icons.arrow_forward_ios),
+                          ),
+                          Divider(),
+                          ListTile(
+                            leading: Icon(Icons.group),
+                            title: Text('Medical records'),
+                            trailing: Icon(Icons.arrow_forward_ios),
+                          ),
+                          Divider(),
+                          ListTile(
+                            leading: Icon(Icons.watch),
+                            title: Text('Remainders'),
+                            trailing: Icon(Icons.arrow_forward_ios),
+                          ),
+                          Divider(),
+                        ],
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                    //color: Colors.blueGrey,
+                    height: 400,
+                    width: double.infinity,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     ));
   }
 }
