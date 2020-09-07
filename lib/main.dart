f946188ecdbb7d2dc275a061bc74c7550670923d
gaurav <270mehta@gmail.com>
2020-09-07 15:24:21 +0530

    Corrected Route

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'ChikitSuck',
-      initialRoute: "chatbot",
+      initialRoute: "/",
       routes: {
         "/": (context) => WelcomeScreen(),
         "chatbot": (context) => ChatMessages(),
